ed164bf92b659eb96132e184cf8b7419c4c94f71
aynur <maksutov_aynur@mail.ru>
2019-07-08 01:53:50 +0300

    topic size

--- test_jpeg.py
+++ test_jpeg.py
@@ -2,12 +2,12 @@ import ctypes as C
 from topic import TopicSpawnMode, Topic
 
 
-class Image1M(C.Structure):
-    _pack_ = 1
-    _fields_=(('i', C.c_uint32), ('t', C.c_uint32), ('data', C.c_char * 500000))
-
-    def __str__(self):
-        return f"Image1M({self.i})"
+# class Image1M(C.Structure):
+#     _pack_ = 1
+#     _fields_=(('i', C.c_uint32), ('t', C.c_uint32), ('data', C.c_char * 500000))
+#
+#     def __str__(self):
+#         return f"Image1M({self.i})"
 
 
 def rm_topic(name=b"Mycooltopic_jpeg0"):
@@ -15,4 +15,4 @@ def rm_topic(name=b"Mycooltopic_jpeg0"):
 
 
 def get_topic(name=b"Mycooltopic_jpeg0"):
-    return Topic(name, C.sizeof(Image1M), 10, TopicSpawnMode.CREATE)
+    return Topic(name, 500000, 10, TopicSpawnMode.CREATE)
