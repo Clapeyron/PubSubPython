3b5bd4abbde826d942138cee28b7bb5a479ad409
aynur <maksutov_aynur@mail.ru>
2019-07-20 15:28:55 +0300

    fix time.time hack

--- test_jpeg_pub.py
+++ test_jpeg_pub.py
@@ -7,6 +7,8 @@ import cv2
 
 if __name__ == "__main__":
     t = tj.get_topic()
+    tt = int(time.time()/1000)*1000
+    print(f"basis: {tt}")
     try:
         cap = cv2.VideoCapture(0)
         cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
@@ -21,7 +23,7 @@ if __name__ == "__main__":
                 break
             bts = cv2.imencode('.jpg', frame)[1].tostring()
             print(len(bts))
-            t.pub(struct.pack("I", i) + struct.pack("I", int((time.time() - 1562539000)*1000000)) + bts)
+            t.pub(struct.pack("I", i) + struct.pack("I", int((time.time() - tt)*1000000)) + bts)
     except Exception as e:
         print("Exception:", e)
         print(traceback.format_exc())
--- test_jpeg_sub.py
+++ test_jpeg_sub.py
@@ -7,13 +7,15 @@ import cv2
 
 if __name__ == "__main__":
     t = tj.get_topic()
+    tt = int(time.time()/1000)*1000
+    print(f"basis: {tt}")
     try:
         while True:
             b = t.sub()
             image = np.asarray(b[8:], dtype="uint8")
             image = cv2.imdecode(image, cv2.IMREAD_COLOR)
             tm = struct.unpack("I", b[4:8])[0]
-            print(int((time.time() - 1562539000)*1000000)-tm)
+            print(int((time.time() - tt)*1000000)-tm)
             cv2.imshow(f'delay', image)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
